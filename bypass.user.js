ed9fd4e88a032a13989e94fd1f4292f68f4340eb
boat <55274516+boat1337@users.noreply.github.com>
2020-07-10 07:17:53 +0200

    yes

--- bypass.user.js
+++ bypass.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SAG Patreon Bypass
 // @namespace    https://github.com/RazerHook
-// @version      1.0
+// @version      1.2
 // @description  Bypass the patreon check on Steam Account Generator
 // @author       RazerHook
 // @match        https://accgen.cathook.club/custom_gen.html
@@ -12,6 +12,8 @@
     'use strict';
     $('#patreon_signin').html('Bypass by <a href="https://razerhook.ml">RazerHook.ML</a> Team! Get good, Get RazerHook!')
     setTimeout(() => {
-        $('#accgen_ui').show();$('#generate_button').show();$('#history_button').show()
+        $('#accgen_ui').show()
+        $('#generate_button').show()
+        $('#history_button').show()
     }, 500)
 })();
